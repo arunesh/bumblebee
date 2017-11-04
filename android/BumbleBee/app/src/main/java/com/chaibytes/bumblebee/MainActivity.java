89b39adf66cb7fd6193a53507067ded6c0727583
Arunesh Mishra <aruneshm@gmail.com>
2017-11-04 12:32:01 -0700

    basic profile UI

--- android/BumbleBee/app/src/main/java/com/chaibytes/bumblebee/MainActivity.java
+++ android/BumbleBee/app/src/main/java/com/chaibytes/bumblebee/MainActivity.java
@@ -1,13 +1,22 @@
 package com.chaibytes.bumblebee;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_starter);
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 }
