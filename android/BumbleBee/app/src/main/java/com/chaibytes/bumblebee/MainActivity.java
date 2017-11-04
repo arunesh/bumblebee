b989a64687d232dda66b6daeee75cc7ed88071ea
Arunesh Mishra <aruneshm@gmail.com>
2017-11-04 12:48:20 -0700

    add maps

--- android/BumbleBee/app/src/main/java/com/chaibytes/bumblebee/MainActivity.java
+++ android/BumbleBee/app/src/main/java/com/chaibytes/bumblebee/MainActivity.java
@@ -1,12 +1,22 @@
 package com.chaibytes.bumblebee;
 
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    public static final String TAG = "BumbleBee";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +28,33 @@ public class MainActivity extends AppCompatActivity {
 
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
+
+        // Get the SupportMapFragment and register for the callback
+        // when the map is ready for use.
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+        // Position the map's camera near Sydney, Australia.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-34, 151)));
     }
 }
